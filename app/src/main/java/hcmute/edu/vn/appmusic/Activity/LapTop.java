0d18c388d0d5c0055e3dcd235274841c4a5a9ae6
taynguyen0206 <phamtrinhtaynguyen2002@gmail.com>
2023-04-11 00:33:18 +0700

    laptop commit

---
+++ app/src/main/java/hcmute/edu/vn/appmusic/Activity/LapTop.java
@@ -0,0 +1,4 @@
+package hcmute.edu.vn.appmusic.Activity;
+
+public class LapTop {
+}
--- app/src/main/java/hcmute/edu/vn/appmusic/Activity/TestConnect.java
+++ app/src/main/java/hcmute/edu/vn/appmusic/Activity/TestConnect.java
@@ -5,4 +5,5 @@ import androidx.appcompat.app.AppCompatActivity;
 public class TestConnect extends AppCompatActivity {
     //tay nguyen
     //tay nguyen 2
+    //tay nguyen 3 - laptop
 }
