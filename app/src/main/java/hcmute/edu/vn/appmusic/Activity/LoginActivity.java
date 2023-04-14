68f07307beb700ee52a830c71edf8322ef820cca
taynguyen0206 <phamtrinhtaynguyen2002@gmail.com>
2023-04-14 12:10:26 +0700

    commit PC login

--- app/src/main/java/hcmute/edu/vn/appmusic/Activity/LoginActivity.java
+++ app/src/main/java/hcmute/edu/vn/appmusic/Activity/LoginActivity.java
@@ -1,13 +1,21 @@
 package hcmute.edu.vn.appmusic.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import hcmute.edu.vn.appmusic.R;
 
@@ -15,6 +23,8 @@ public class LoginActivity extends AppCompatActivity {
     EditText txtUserName, txtPassword;
     Button btnSignIn;
     private Button btnfacebook, btngmail;
+
+    private FirebaseAuth mAuth;
 
 
     @Override
@@ -71,19 +81,59 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void gmail() {
+        Intent intent =new Intent(this, GoogleAccount.class);
+        intent.putExtra("getToken",false);
+        startActivityForResult(intent,10);
     }
 
     private void facebook() {
+        Intent intent =new Intent(this, FacebookAccount.class);
+        intent.putExtra("getToken",false);
+        startActivityForResult(intent,10);
     }
 
     private void forgetPass() {
     }
-
+    //đăng nhập thường
     private void login() {
-    }
+        //final để đánh dấu đây là biến hằng có giá trị không thay đổi sau khi khởi tạo
+        final String UserName = txtUserName.getText().toString();
+        String PassWord = txtPassword.getText().toString();
 
+        //kiểm tra dữ liệu người dùng nhập vào
+        if(UserName.isEmpty()){
+            txtUserName.setError("Vui lòng nhập tên đăng nhập!");
+            txtUserName.requestFocus(); // đưa trỏ tới ô cần nhập
+        }else if(PassWord.isEmpty()){
+            txtPassword.setError("Vui lòng nhập password");
+            txtPassword.requestFocus();
+        }else if(UserName.isEmpty() && PassWord.isEmpty()){
+            Toast.makeText(LoginActivity.this, "Các ô không được để trống!", Toast.LENGTH_SHORT).show();
+        }
+        //khi nhập đầy đủ thông tin
+        else if (!(UserName.isEmpty() && PassWord.isEmpty())){
+            mAuth.signInWithEmailAndPassword(UserName,PassWord).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(!task.isSuccessful()){
+                        Toast.makeText(LoginActivity.this, "Đăng nhập thất bại, vui lòng kiểm tra lại!", Toast.LENGTH_SHORT).show();
+                    }
+                    //đăng nhập thành công
+                    else{
+                        Toast.makeText(LoginActivity.this, "Đăng nhập thành công!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+        else{
+            Toast.makeText(LoginActivity.this, "Có lỗi xảy ra!", Toast.LENGTH_SHORT).show();
+        }
+    }
+    //Hàm đăng ký
     private void signUp() {
+        startActivity(new Intent(LoginActivity.this, RegistrationActivity.class));
+        overridePendingTransition(R.anim.slide_out_left, R.anim.slide_in_right);
+        finish();
     }
-
 
 }
